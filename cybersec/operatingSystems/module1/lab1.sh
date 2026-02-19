3899e8c67006e772e637e9f84c8b43601833e281
Josue Vargas <josuhelvargas@gmail.com>
2026-02-19 12:03:46 -0600

    Changes to move projects to leanrn c++, c operating systems architecture AND  hacking cybersec  has been added

---
+++ cybersec/operatingSystems/module1/lab1.sh
@@ -0,0 +1,12 @@
+#Cada comando realiza una llamada (syscall ) para realizar una poperacion a nivel de kernel
+# 🟦 LAB 1 — Entendiendo la Arquitectura Linux (Desde Dentro)
+# 🧠 Objetivo del bloque
+
+# Al terminar estos ejercicios debes poder responder:
+
+# ¿Cómo pasa una instrucción de userland al kernel?
+# ¿Qué es realmente un proceso?
+# ¿Cómo interactúa la memoria con el kernel?
+# ¿Cómo el sistema expone información interna vía pseudo-filesystems?
+# ¿Dónde viven las superficies de ataque?
+
